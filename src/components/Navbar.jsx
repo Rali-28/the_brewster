b404b0f7b0e642683005479a0f92033bd6f5b0be
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2022-12-04 20:51:19 +0800

    Navlinks
    Rendered navlinks text and format.
    Added divs for logo and map

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,43 @@ import { navLinks } from '../constants'
 const Navbar = () => (
   
   /* Navbar Container */
-  <nav>
+  <nav className='items-center justify-between w-full py-6 clex'>
     
     {/* Render Desktop navLinks */}
     <div>
-      <ul>
+      <ul className='items-center justify-start flex-1 pt-4 list-none sm:flex'>
         {navLinks.map((nav, index) => (
           <li
-          key={nav.id}>
+          key={nav.id}
+          className={`font-primary_md
+          tracking-widest
+          text-[16px]
+          ${index === navLinks.length -1 ? 'mr-0' : 'mr-10'}`}>
             <a href={`#${nav.id}`}>
               {nav.title}
             </a>
           </li>
         ))}
       </ul>
+
+      {/* Mobile view Navbar */}
+      <div>
+        
+      </div>
+      {/* End Mobile View */}
+
+      {/* Middle Logo */}
+      <div>
+
+      </div>
+      {/* End Logo */}
+
+      {/* Location GMap */}
+      <div>
+          
+      </div>
+      {/* End Map */}
+
     </div>
     {/* End Desktop navLinks */}
 
