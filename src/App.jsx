42d4d55d43adee492a769bbf7d0f482134d0d61f
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2022-12-04 12:04:53 +0800

    react components
    Create react components and page sections.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { AboutUs, Contacts, Footer, Landing, Menu, Navbar } from './components'
 
 const App = () => (
-  <div>
+
+  /* Main Div Container */
+  <div className='bg-primary'>
     
+    {/* Navbar */}
+    <div>
+      <div>
+        <Navbar />
+      </div>
+    </div>
+
+    {/* Landing Page */}
+    <div>
+      <div>
+        <Landing />
+      </div>
+    </div>
+
+    {/* Content */}
+    <div>
+      <div>
+        <AboutUs />
+        <Menu />
+        <Contacts />
+      </div>
+    </div>
+
+    {/* Footer */}
+    <div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+
   </div>
+  /* End Main */
 )
 
 export default App
---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import AboutUs from './AboutUs';
+import Contacts from './Contacts';
+import Footer from './Footer';
+import Menu from './Menu';
+import Navbar from './Navbar';
+import Landing from './Landing';
+
+export
+{
+    AboutUs,
+    Contacts,
+    Footer,
+    Menu,
+    Navbar,
+    Landing
+};
