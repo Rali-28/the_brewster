4ff8502ea536aed4c07b41f679cb8fc54d351f62
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2022-12-04 18:38:19 +0800

    created constants/index.js
    Created constants/index.js for rendering static texts. navlinks included

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+/* Static Texts for Rendering */
+
+export const navLinks = [
+    {
+        id: 'home',
+        title: 'Home',
+    },
+    {
+        id: 'about-us',
+        title: 'About Us',
+    },
+    {
+        id: 'menu',
+        title: 'Menu',
+    },
+    {
+        id: 'contacts',
+        title: 'Contacts',
+    },
+];
