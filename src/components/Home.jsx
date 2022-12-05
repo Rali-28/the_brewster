1f516771f801897d969d5319169b0e366dd9d401
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2022-12-05 13:07:24 +0800

    Created home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { logo } from '../assets'
-import styles from '../style'
 
 const Home = () => (
-  <section id='home'>
-    img
+  <section id='home' className='flex flex-col items-center justify-center'>
+    <div>
+      <img src={logo} alt="logo" />
+    </div>
+    <div>
+      <h1 className='text-dimBlack font-primary_italic'>
+        Created by LogoDesign.net 
+      </h1>
+    </div>
+    
   </section>  
 )
 
