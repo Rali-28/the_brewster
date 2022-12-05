77af4ce56ba22a0477510124f8e72aea23bc1559
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2022-12-05 09:04:08 +0800

    Added burger svg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,10 @@ const Navbar = () => (
       {/* Mobile View Nav */}
       <div className='order-first col-span-1 md:hidden'> 
         <div>
-                
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+</svg>
+
         </div>
       </div>
       {/* End Mobile View */}
