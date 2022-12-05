ca566fd7dd61b930fc38179c689fab3e8a43a2c0
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2022-12-05 13:08:07 +0800

    Update NavLogo.jsx

--- src/components/NavLogo.jsx
+++ src/components/NavLogo.jsx
@@ -3,7 +3,7 @@ import { logo_sm } from '../assets';
 
 const NavLogo = () => (
     <div>
-        <a href="#">
+        <a href="home">
             <div className={`justify-center flex bg-white rounded-full w-[95px] h-[95px] items-center`}>
         
             <img src={logo_sm} alt='logo' className='w-[50px] h-[50px]' />
