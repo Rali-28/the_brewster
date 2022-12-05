aa9024e5c2ca83e98ee4f98a623fe5522bea1efd
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2022-12-05 13:31:21 +0800

    Update index.js for menu

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,3 +18,64 @@ export const navLinks = [
         title: 'Contacts',
     },
 ];
+
+export const menu =
+[
+    {
+        title: 'Iced/Hot Coffee',
+        product: [
+            {
+                name: 'Espresso',
+            },
+            {
+                name: 'Americano',
+            },
+            {
+                name: 'Cafe Latte',
+            },
+            {
+                name: 'Cappuccino',
+            },
+            {
+                name: 'Machiatto',
+            },
+        ],
+    },
+    {
+        title: 'Iced/Hot Non-Coffee',
+        product: [
+            {
+                name: 'Chocolate',
+            },
+            {
+                name: 'Matcha Latte',
+            },
+            {
+                name: 'Peach Mango',
+            },
+            {
+                name: '',
+            },
+            {
+                name: 'Water',
+            },
+        ],
+    },
+    {
+        title: 'Pastries',
+        product: [
+            {
+                name: 'Brownies',
+            },
+            {
+                name: 'Caramel Bar',
+            },
+            {
+                name: 'Blueberry Cheesecake',
+            },
+            {
+                name: 'Calamansi Cheesecake',
+            },
+        ],
+    },
+];
