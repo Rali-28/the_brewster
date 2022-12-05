9d4a1fc3b1e0916d76c06922b2b942be480f57ce
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2022-12-05 08:30:48 +0800

    Desktop View Navbar
    Finalized the desktop view of the navbar

--- src/components/NavLogo.jsx
+++ src/components/NavLogo.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { logo_sm } from '../assets';
-import styles from '../style';
 
 const NavLogo = () => (
-    <div className={`justify-center flex bg-white rounded-full w-[100px] h-[100px] items-center`}>
+    <div className={`justify-center flex bg-white rounded-full w-[95px] h-[95px] items-center`}>
         <a href="#">
             <img src={logo_sm} alt='logo' className='w-[50px] h-[50px]' />
         </a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,57 +9,66 @@ import { layout } from '../style'
 */
 
 const Navbar = () => (
-  
+ 
   /* Navbar Container */
-  <nav className='flex items-center justify-between w-full py-6'>
-  
-    {/* Render Desktop navLinks */}
-    <div className='flex justify-start w-[1/3]'>
-      <ul className='hidden pt-4 list-none items-left sm:flex'>
-        {navLinks.map((nav, index) => (
-          <li
-          key={nav.id}
-          className={`font-primary_md
-          tracking-widest
-          text-[18px]
-          ${index === navLinks.length -1 ? 'mr-0' : 'mr-5'}`}>
-            <a href={`#${nav.id}`}>
-              {nav.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+  <nav className='grid items-center grid-cols-3 gap-5'>
 
-    {/* Middle Logo */}
-    <div className='flex justify-center w-[1/3]'>
-      <NavLogo />
-    </div>
-    {/* End Logo */}
+      {/* Desktop View Navlinks */}
 
-    
+        {/* Render Links */}
+        <div className='flex items-center justify-start'>
+          <ul className='items-center hidden text-center list-none items-left md:flex'>
+            {navLinks.map((nav, index) => (
+              <li
+              key={nav.id}
+              className={`font-primary_md
+              tracking-widest
+              text-[18px]
+              ${index === navLinks.length -1 ? 'mr-0' : 'mr-5'}`}>
+                <a href={`#${nav.id}`}>
+                  {nav.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* End Render */}
 
-    {/* Location GMap */}
-    <div className='justify-end hidden pt-4 sm:flex place-items-center w-[1/3]'>
 
-      <div>
-        <a src='#' className={`items-center items cursor-pointer sm:flex ${layout.map_text}`}>
+      {/* End Desktop View */}
 
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='fill-none w-[26px] h-[26px] stroke-[1.5] stroke-black mr-2'>
-
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-          </svg>
-          
-            The Brewster's <br />
-            Location
+      {/* Center Logo */}
+      <div className='content-center'>
+        <a href="#">
+          <div className='flex justify-center py-2'>
+            <NavLogo />
+          </div>
         </a>
-      </div>
-    </div>
-    {/* End Map */}
+      </div >
+      {/* End Center */}
+
+
+      {/* Location Button */}
+      <div className='justify-end hidden md:flex'>
+          <div>
+            <a src='#' className={`items-center items cursor-pointer sm:flex ${layout.map_text}`}>
+
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='fill-none w-[26px] h-[26px] stroke-[1.5] stroke-black mr-2'>
+
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+              </svg>
+    
+                The Brewster's <br />
+                Location
+            </a>
+          </div>
+        </div>
+      {/* End Location */}
 
   </nav>
-  /* End Navbar */
+  /* End Nav */
+
 )
 
 
